adb35a61419a991675bb822cdbe8cc722545fa20
RESUELVE\resparza <reynaldoeg_esparza@hotmail.com>
2018-02-27 19:07:04 -0600

    fix: secure_asset

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -48,11 +48,11 @@
 @section('page_js')
     <!-- google maps api -->
     <script src="https://maps.googleapis.com/maps/api/js?sensor=true&key=AIzaSyAEjp3kq9YA0EF8nGjKPcDPlYQbpeT5Ljk"></script>
-    <script src="{{ secure_asset('js/pages/maps.js', \App::environment() == 'production')}}"></script>
+    <script src="{{ asset('js/pages/maps.js', \App::environment() == 'production')}}"></script>
     <!-- script src="assets/vendor_components/gmaps/jquery.gmaps.js"></script -->
     <!-- DataTables -->
-    <script src="{{ secure_asset('assets/vendor_components/datatables.net/js/jquery.dataTables.min.js', \App::environment() == 'production') }}"></script>
-    <script src="{{ secure_asset('assets/vendor_components/datatables.net-bs/js/dataTables.bootstrap.min.js', \App::environment() == 'production') }}"></script>
+    <script src="{{ asset('assets/vendor_components/datatables.net/js/jquery.dataTables.min.js', \App::environment() == 'production') }}"></script>
+    <script src="{{ asset('assets/vendor_components/datatables.net-bs/js/dataTables.bootstrap.min.js', \App::environment() == 'production') }}"></script>
     <!-- Moment -->
-    <script src="{{ secure_asset('assets/vendor_components/moment/moment.js', \App::environment() == 'production')}}"></script>
+    <script src="{{ asset('assets/vendor_components/moment/moment.js', \App::environment() == 'production')}}"></script>
 @endsection
