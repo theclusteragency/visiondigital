b6eb0600735115787049e71c320994a277f73556
RESUELVE\resparza <reynaldoeg_esparza@hotmail.com>
2018-03-01 21:18:38 -0600

    favicon

--- resources/views/layouts/rafagana.blade.php
+++ resources/views/layouts/rafagana.blade.php
@@ -9,11 +9,11 @@
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     
     <link rel="icon" href="favicon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon.png">
+    <link rel="apple-icon" sizes="120x120" href="/apple-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/manifest.json">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="mask-icon" href="/ms-icon-150x150.png" color="#5bbad5">
     <meta name="theme-color" content="#ffffff">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
